989a9ee80b02cf6f65a6e2960f42912f9b7b843a
SHuzzain <shahinhuzzain@gmail.com>
2023-10-02 17:04:28 +0530

    signin page re-desgin

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,14 +1,22 @@
 import { updateProfile } from "firebase/auth";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { auth, db, signIn, signUp } from "../../firebase";
 import Loading from "react-loading";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useDispatch } from "react-redux";
 import { tokenCheck } from "../../feature/userSlice";
+import { useForm, } from "react-hook-form";
+
 
 function SignIn() {
   const navigate = useNavigate();
+  const { getValues, setValue, handleSubmit, formState: { errors }, register, setError, reset, watch } = useForm({
+    mode: 'onSubmit',
+    shouldUseNativeValidation: true
+
+  })
+
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "signIn";
@@ -26,87 +34,19 @@ function SignIn() {
       },
   };
 
-  const [formInput, setFormeInput] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
+
 
   // handle the user login or signUp
-  const handleSumbit = async (e) => {
-    e.preventDefault();
+  const fromSumbit = async (e) => {
     setLoading(true);
+    const formInput = getValues()
     switch (isLogin) {
       case true:
-        // login handle
-        if (!formInput.email || !formInput.password) {
-
-          alert("please fill the empty field");
-        } else {
-          try {
-            //  backend
-            await signIn(formInput.email, formInput.password)
-              .then(() => {
-                setFormeInput(() => ({
-                  username: "",
-                  email: "",
-                  password: "",
-                }));
-                localStorage.setItem(
-                  "accessToken",
-                  auth.currentUser.refreshToken
-                );
-                dispatch(
-                  tokenCheck({
-                    accessToken: auth.currentUser.refreshToken,
-                  })
-                );
-                const expiryTime = new Date();
-                expiryTime.setHours(expiryTime.getHours() + 24);
-                localStorage.setItem("expiration", expiryTime.toISOString());
-              })
-              .then(() => {
-                navigate("..");
-              });
-          } catch (error) {
-            const result = await error.message.split(" ").slice(-1);
-
-            if (result[0].includes("password")) {
-              setFormeInput((preVal) => ({
-                ...preVal,
-                password: "",
-              }));
-              alert("wrong passowrd please check the password");
-            } else {
-              alert(result[0]);
-            }
-          } finally {
-            setLoading(false);
-          }
-        }
-        break;
-      case false:
-        //  signUp
-        if (!formInput.email || !formInput.username || !formInput.password) {
-          alert("please fill the empty field");
-        } else {
-          // backend call
-          await signUp(formInput.email, formInput.password)
-            .then(async () => {
-              await updateProfile(auth.currentUser, {
-                displayName: formInput.username,
-              });
-              await setDoc(doc(db, "user", auth.currentUser.uid), {
-                uid: auth.currentUser.uid,
-                displayName: formInput.username,
-                email: formInput.email,
-                photoUrl: "",
-                online: true,
-                bookmark: [],
-                timestamp: serverTimestamp(),
-              });
-            })
+        try {
+          //  backend
+          await signIn(formInput.email, formInput.password)
             .then(() => {
+
               localStorage.setItem(
                 "accessToken",
                 auth.currentUser.refreshToken
@@ -119,58 +59,98 @@ function SignIn() {
               const expiryTime = new Date();
               expiryTime.setHours(expiryTime.getHours() + 24);
               localStorage.setItem("expiration", expiryTime.toISOString());
-
-              setFormeInput(() => ({
-                email: "",
-                username: "",
-                password: "",
-              }));
-              navigate("..");
             })
-            .finally(() => {
-              setLoading(false);
+            .then(() => {
+              navigate("..");
             });
+        } catch (error) {
+          const result = await error.message.split(" ").slice(-1);
+
+          if (result[0].includes("password")) {
+            setValue('password', '')
+            setError("username", {
+              type: "manual",
+              message: "oops! your password is wrong take breath try again",
+            })
+          } else {
+            alert(result[0]);
+          }
+        } finally {
+          setLoading(false);
+          reset()
         }
 
+        break;
+      case false:
+        //  signUp
+        // backend call
+        await signUp(formInput.email, formInput.password)
+          .then(async () => {
+            await updateProfile(auth.currentUser, {
+              displayName: formInput.username,
+            });
+            await setDoc(doc(db, "user", auth.currentUser.uid), {
+              uid: auth.currentUser.uid,
+              displayName: formInput.username,
+              email: formInput.email,
+              photoUrl: "",
+              online: true,
+              bookmark: [],
+              timestamp: serverTimestamp(),
+            });
+          })
+          .then(() => {
+            localStorage.setItem(
+              "accessToken",
+              auth.currentUser.refreshToken
+            );
+            dispatch(
+              tokenCheck({
+                accessToken: auth.currentUser.refreshToken,
+              })
+            );
+            const expiryTime = new Date();
+            expiryTime.setHours(expiryTime.getHours() + 24);
+            localStorage.setItem("expiration", expiryTime.toISOString());
+            navigate("..");
+          })
+          .finally(() => {
+            reset()
+            setLoading(false);
+          });
         break;
       default:
         break;
     }
   };
-  //  update current word in user type in input field
-  const handleInputChange = (e) => {
-    const currentFieldName = e.target.name;
-    setFormeInput((preVal) => ({
-      ...preVal,
-      [currentFieldName]: e.target.value,
-    }));
-  };
+
+
 
   return (
-    <div className="h-screen  bg-white px-2 py-1">
-      <div className="min-w-[auto] max-w-[25rem] mx-auto  items-center  flex flex-col justify-center">
+    <div div className="h-screen flex items-center justify-center bg-white px-2 py-1 bg-gradient-to-c from-yellow-300 from-50% to-black to-50%" >
+      <div className={`w-[28rem] border ${Object.keys(errors).length ? 'border-red-400' : 'border-white'} rounded p-3`}>
+
         <section className="flex flex-col gap-3 w-full">
-          <h2 className="my-2 font-semibold text-base">Shareme</h2>
-          <h2 className="font-bold text-3xl">{searPathMode.pathMode.name}</h2>
-          <p className="text-gray-500 text-xs font-medium mb-3 text-center">
+          <h2 className="my-2 font-semibold text-base mix-blend-difference text-yellow-300">Shareme</h2>
+          <h2 className="font-bold text-3xl mix-blend-difference text-yellow-300">{searPathMode.pathMode.name}</h2>
+          <p className="text-gray-400 mix-blend-difference text-xs font-medium mb-3 text-center">
             {isLogin
               ? "Welcome back "
               : "See your growth and get consulting support!"}
           </p>
         </section>
-
-        <section className="flex gap-1 items-center border rounded-full border-black p-3 w-full justify-center hover:bg-blue-500 transition-all duration-200 cursor-pointer hover:text-white text-base font-semibold active:scale-[0.98] hover:shadow-black active:shadow-sm">
+        <section className="flex gap-1 mix-blend-difference items-center border rounded-full border-black p-3 w-full justify-center hover:border-yellow-300  transition-colors duration-300 cursor-pointer  text-base font-semibold active:scale-[0.98] hover:shadow-black active:shadow-sm">
           <img
-            className="w-[25px]"
+            className="w-6"
             src="https://companieslogo.com/img/orig/GOOG-0ed88f7c.png?t=1633218227"
             alt=""
           />
-          <p>Sign up with Google</p>
+          <p className="max-sm:text-sm mix-blend-difference text-yellow-300">Sign up with Google</p>
         </section>
 
-        <section className="max-h-[20rem] min-h-[auto] my-4 font-medium text-[12px] gap-1 text-gray-500 flex items-center w-full">
+        <section className="max-h-[20rem] min-h-[auto] my-4 font-medium text-[12px] gap-1 text-gray-400 flex items-center w-full">
           <p className="w-full h-[0.7px] bg-gray-500" />{" "}
-          <span className="whitespace-nowrap">
+          <span className="whitespace-nowrap mix-blend-difference">
             {" "}
             or {searPathMode.pathMode.name} with Email
           </span>
@@ -178,45 +158,49 @@ function SignIn() {
         </section>
 
         <form
-          onSubmit={handleSumbit}
+          onSubmit={handleSubmit(fromSumbit)}
           className="w-full flex [&>label>p]:text-sm [&>label>section>p]:text-sm  flex-col gap-3 [&>label>input]:loginForm font-medium"
         >
           {!isLogin && (
             <label htmlFor="name" className="sign_user">
-              <p>Name*</p>
+              <p className="mix-blend-difference text-yellow-300">Name*</p>
               <input
-                required
-                onChange={(e) => handleInputChange(e)}
-                value={formInput.username}
+                {...register('username', {
+                  required: 'This field required'
+                })}
                 type="text"
-                name="username"
                 id="name"
+                className={` ${errors?.username ? 'outline-red-500' : "outline-blue-500"}`}
               />
             </label>
           )}
 
           <label htmlFor="email">
-            <p>Email*</p>
-            <input
-              required
-              onChange={(e) => handleInputChange(e)}
-              value={formInput.email}
-              type="email"
-              name="email"
-              id="email"
-            />
+            <p className="mix-blend-difference text-yellow-300">Email*</p>
+            <input {...register("email", {
+              required: "Please Enter Your Email!",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Please Enter A Valid Email!"
+              }
+            })} type="email" id="email"
+              className={`${errors?.email ? 'outline-red-500' : "outline-blue-500"}`} />
           </label>
 
           <label htmlFor="password">
-            <p>Password*</p>
+            <p className="mix-blend-difference text-yellow-300">Password*</p>
             <input
-              required
-              onChange={(e) => handleInputChange(e)}
-              value={formInput.password}
-              minLength="6"
+
+              {...register('password', {
+                minLength: {
+                  value: 6,
+                  message: 'Password must be at least 8 characters long'
+                },
+                required: 'This field required'
+              })}
               type="password"
-              name="password"
               id="password"
+              className={`${errors?.password ? 'outline-red-500' : "outline-blue-500"}`}
             />
           </label>
 
@@ -224,14 +208,14 @@ function SignIn() {
             <label htmlFor="terms">
               <section className="flex gap-2 ">
                 <input
-                  className="w-3 cursor-pointer"
+                  className="w-3 cursor-pointer accent-yellow-300"
                   type="checkbox"
                   name=""
                   id="terms"
                 />
-                <p>
+                <p className="mix-blend-difference text-yellow-300 ">
                   I agreee to the{" "}
-                  <span className="text-blue-600 hover:text-blue-500 cursor-pointer">
+                  <span className=" mix-blend-normal text-white  hover:text-blue-500 cursor-pointer">
                     Terms & Conditions
                   </span>
                 </p>
@@ -257,7 +241,7 @@ function SignIn() {
           </button>
         </form>
         <section className="w-full text-start mt-3 ">
-          <p className="[&>a]:text-blue-500 [&>a]:font-medium text-sm">
+          <p className="[&>a]:text-white text-yellow-300 mix-blend-difference [&>a]:font-medium text-sm">
             {searPathMode.pathMode.message}
             <Link to={`?mode=${isLogin ? "signUp" : "signIn"}`}>
               {!isLogin ? " SignIn" : " SignUp"}
@@ -265,7 +249,8 @@ function SignIn() {
           </p>
         </section>
       </div>
-    </div>
+
+    </div >
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-c': 'linear-gradient(30deg, var(--tw-gradient-stops))',
+      }
+    },
   },
   plugins: ['tailwind-scrollbar-hide'],
 }
